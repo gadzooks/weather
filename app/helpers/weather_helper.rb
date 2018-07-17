31e75e5007c69dcdf69fe78dbe0f637760190056
Amit Karwande <karwande@gmail.com>
2018-07-17 17:14:33 +0530

    added some colors to the icons and fixed forecast table a bit

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -1,2 +1,41 @@
 module WeatherHelper
+  ICON_MAPPING = {
+    'clear-day' => 'day-sunny',
+    'clear-night' => 'night-clear',
+    'rain' => 'rain',
+    'snow' => 'snow',
+    'sleet' => 'sleet',
+    'wind' => 'strong-wind',
+    'fog' => 'fog',
+    'cloudy' => 'cloudy',
+    'partly-cloudy-day' => 'day-cloudy',
+    'partly-cloudy-night' => 'night-partly-cloudy',
+    'hail' => 'hail',
+    'thunderstorm' => 'thunderstorm',
+    'tornado' => 'tornado',
+  }
+
+  def icon_class(icon, precipitation)
+    mapping = ICON_MAPPING[icon] || icon
+    addional_class = ''
+    if mapping == 'day-cloudy'
+      addional_class =
+        case
+        when precipitation < 10
+          'day-cloudy-10'
+        when precipitation < 20
+          'day-cloudy-20'
+        when precipitation < 30
+          'day-cloudy-30'
+        else
+          'day-cloudy-100'
+        end
+    end
+
+    "wi weather-icon wi-#{mapping} #{mapping} #{addional_class}"
+  end
+
+  def precipitation(precipitation)
+    precipitation.round.to_s + '%'
+  end
 end
