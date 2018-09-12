059dd7659ce51edd7fb5726a9e73fad23e7a9f45
Amit Karwande <karwande@gmail.com>
2018-09-12 16:49:49 -0700

    minor UI updates

--- app/helpers/weather_helper.rb
+++ app/helpers/weather_helper.rb
@@ -16,17 +16,13 @@ module WeatherHelper
   }
 
   def icon_class(icon, precipitation)
+    precipitation = precipitation.to_f
     mapping = ICON_MAPPING[icon] || 'na'
     addional_class = ''
     if mapping == 'day-cloudy'
       addional_class =
-        case
-        when precipitation < 10
+        if precipitation < 0.3
           'day-cloudy-10'
-        when precipitation < 20
-          'day-cloudy-20'
-        when precipitation < 30
-          'day-cloudy-30'
         else
           'day-cloudy-100'
         end
