302b98b25b32afd1be40d309481426b01dd14480
gadzooks <karwande@gmail.com>
2020-04-11 18:57:43 -0700

    add WTA trip reports

--- app/models/latitude_longitude_by_region.rb
+++ app/models/latitude_longitude_by_region.rb
@@ -6,7 +6,7 @@ class LatitudeLongitudeByRegion
   YAML_REGION_DATA = YAML::load(File.open('app/models/data/wta_search_regions.yml'))
   YAML_LOCATION_DATA = YAML::load(File.open('app/models/data/regional_lat_long.yml'))
 
-  Location = Struct.new(:name, :description, :latitude, :longitude, :region, :sub_region) do
+  Location = Struct.new(:name, :description, :latitude, :longitude, :region_obj, :sub_region) do
     def ==(other)
       self.class == other.class && other.name == name
     end
@@ -16,6 +16,17 @@ class LatitudeLongitudeByRegion
     def hash
       name.hash
     end
+
+    def region
+      region_obj ? region_obj.name : nil
+    end
+
+    def wta_trip_report_url
+      if region_obj && region_obj.respond_to?(:search_key)
+        region_obj.wta_trip_report_url(sub_region)
+      end
+    end
+
   end
 
   Region = Struct.new(:name, :search_key, :description) do
@@ -29,8 +40,9 @@ class LatitudeLongitudeByRegion
       name.hash
     end
 
-    def wta_trip_report_url
-      "https://www.wta.org/go-outside/trip-reports/tripreport_search?title=&region=#{search_key}&subregion=all&searchabletext=&author=&startdate=&_submit=&enddate=&_submit=&month=all&format=list&filter=Search"
+    def wta_trip_report_url(subregion_key = nil)
+      subregion_query_str = subregion_key.blank? ? 'all' : subregion_key
+      "https://www.wta.org/go-outside/trip-reports/tripreport_search?title=&region=#{search_key}&subregion=#{subregion_query_str}&searchabletext=&author=&startdate=&_submit=&enddate=&_submit=&month=all&format=list&filter=Search"
     end
   end
 
@@ -42,18 +54,20 @@ class LatitudeLongitudeByRegion
     @locations = {}
     @regions = ActiveSupport::OrderedHash.new
 
-    YAML_LOCATION_DATA.each do |y|
-      name = y.first
-      hsh = y.second
-      @locations[name] = Location.new(name, hsh['description'], hsh['latitude'],
-                                      hsh['longitude'], hsh['region'], hsh['sub_region'])
-    end
-
     YAML_REGION_DATA.each do |y|
       name = y.first
       hsh = y.second
       Rails.logger.info y.inspect
       @regions[name] = Region.new(name, hsh['search_key'], hsh['description'])
+    end
+
+    YAML_LOCATION_DATA.each do |y|
+      name = y.first
+      hsh = y.second
+      region = @regions[hsh['region']]
+      Rails.logger.info "Region for location #{name} is #{region}"
+      @locations[name] = Location.new(name, hsh['description'], hsh['latitude'],
+                                      hsh['longitude'], region, hsh['sub_region'])
     end
 
   end
